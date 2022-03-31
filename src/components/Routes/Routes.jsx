1be4e9624a859d0f9bd6677691a899d617164ef8
Namrata-J <namratajain29001@gmail.com>
2022-03-31 23:52:10 +0530

    update: added routes for new pages

--- src/components/Routes/Routes.jsx
+++ src/components/Routes/Routes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { LandingPage, HomePage, LabelsPage, ArchivePage, TrashPage, ProfilePage } from "../../pages";
+import { LandingPage, HomePage, LabelsPage, ArchivePage, TrashPage, ProfilePage, LogInPage, LogOutPage, SignUpPage } from "../../pages";
 
 const RoutesComponent = () => {
     return (
@@ -10,6 +10,9 @@ const RoutesComponent = () => {
             <Route path="/archive" element={ <ArchivePage /> } />
             <Route path="/trash" element={ <TrashPage /> } />
             <Route path="/profile" element={ <ProfilePage /> } />
+            <Route path="/logIn" element={ <LogInPage /> } />
+            <Route path="/logOut" element={ <LogOutPage /> } />
+            <Route path="/signUp" element={ <SignUpPage /> } />
         </Routes>
     );
 }
