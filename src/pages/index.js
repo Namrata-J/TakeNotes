bcf84e15780d1a22d52d42fd713014579d91c0db
Namrata-J <namratajain29001@gmail.com>
2022-03-31 16:25:49 +0530

    common index file for exporting pages to routes component

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import { LandingPage } from "./Landing/LandingPage";
+import { HomePage } from "./Home/HomePage";
+import { LabelsPage } from "./Labels/LabelsPage";
+import { ArchivePage } from "./Archive/ArchivePage";
+import { TrashPage } from "./Trash/TrashPage";
+import { ProfilePage } from "./Profile/ProfilePage";
+
+export { LandingPage, HomePage, LabelsPage, ArchivePage, TrashPage, ProfilePage };
