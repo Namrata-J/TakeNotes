9467a588da95bb925f717fd6ef6ec21652b358de
Namrata-J <namratajain29001@gmail.com>
2022-04-01 00:16:36 +0530

    fix: fixed the prefix for all classnames

--- src/pages/Landing/LandingPage.js
+++ src/pages/Landing/LandingPage.js
@@ -3,21 +3,21 @@ import { Link } from "react-router-dom";
 
 const LandingPage = () => {
     return (
-        <div className="ap_landing-page">
-            <div className="ap_landing-page-content-container">
-                <div className="ap_landing-page-content a-tl">
+        <div className="tn_landing-page">
+            <div className="tn_landing-page-content-container">
+                <div className="tn_landing-page-content a-tl">
                     <h2>TakeNotes</h2>
-                    <div className="ap_app-subtitle">
+                    <div className="tn_app-subtitle">
                         <h4>Meet your modern</h4>
-                        <h4 className="ap_highlighted-subtitle">Note Taking App</h4>
+                        <h4 className="tn_highlighted-subtitle">Note Taking App</h4>
                     </div>
-                    <p className="ap_app-description">Manage your daily tasks and workflow in a better way and
+                    <p className="tn_app-description">Manage your daily tasks and workflow in a better way and
                         boost your efficiency without any efforts.
                     </p>
                     <Link to="/signUp">
-                        <button className="et_p-simple-btn primary-color btn">Join Now</button>
+                        <button className="tn_p-simple-btn primary-color btn">Join Now</button>
                     </Link>
-                    <Link to="logIn" className="ap_landing-page-login">
+                    <Link to="logIn" className="tn_landing-page-login">
                         <p>Already have an account?</p>
                     </Link>
                 </div>
