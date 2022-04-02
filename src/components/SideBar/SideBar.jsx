5298fc6200c76e8eacc00dd381b6ffee803bec96
Namrata-J <namratajain29001@gmail.com>
2022-04-02 11:47:47 +0530

    feat: sidebar component

---
+++ src/components/SideBar/SideBar.jsx
@@ -0,0 +1,55 @@
+import "./sideBar.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { MdLabelOutline } from "react-icons/md";
+import { BsArchive, BsTrash } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
+import { Link } from "react-router-dom";
+
+const sideBarLinksArr = [
+    {
+        pagePath: "/home",
+        icon: <AiOutlineHome />,
+        pageName: "Home"
+    },
+    {
+        pagePath: "/labels",
+        icon: <MdLabelOutline />,
+        pageName: "Labels"
+    },
+    {
+        pagePath: "/archive",
+        icon: <BsArchive />,
+        pageName: "Archive"
+    },
+    {
+        pagePath: "/trash",
+        icon: <BsTrash />,
+        pageName: "Trash"
+    },
+    {
+        pagePath: "/profile",
+        icon: <CgProfile />,
+        pageName: "Profile"
+    }
+];
+
+const SideBar = () => {
+    return (
+        <div className="tn_sideBar-component">
+            <div className="tn_sideBar-content a-tl">
+                {
+                    sideBarLinksArr.map((page, index) => {
+                        return (
+                            <Link to={ page.pagePath } className="tn_each-content" key={ index }>
+                                <i className="tn_sideBar-icons">{ page.icon }</i>
+                                <div>{ page.pageName }</div>
+                            </Link>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+
+export { SideBar };
