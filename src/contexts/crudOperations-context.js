c1a6da90a5b998121ed313dc51d78847af90e023
Namrata-J <namratajain29001@gmail.com>
2022-04-05 20:43:12 +0530

    feat: pin unpin functionality added

--- src/contexts/crudOperations-context.js
+++ src/contexts/crudOperations-context.js
@@ -8,7 +8,7 @@ const CrudOperationsProvider = ({ children }) => {
     const notesReducer = ( stateOfNotes, action ) => {
         switch(action.type){
             case "ADD_NOTE": 
-                return [ ...stateOfNotes, { _id: uuid(), title: action.payload._title, description: action.payload._description, currDate: new Date(), canEdit: false }]
+                return [ ...stateOfNotes, { _id: uuid(), title: action.payload._title, description: action.payload._description, currDate: new Date(), canEdit: false, isPinned: false }]
             case "EDIT_NOTE":
                 return [ ...stateOfNotes.map(currNote => currNote._id === action.payload._id? {...currNote, canEdit: true} : currNote ) ]
             case "SAVE_NOTE":
@@ -19,6 +19,10 @@ const CrudOperationsProvider = ({ children }) => {
                 return [ ...stateOfNotes.map(currNote => currNote._id === action.payload.note._id? {...currNote, description: action.payload.value} : currNote ) ]
             case "DELETE_NOTE":
                 return [ ...stateOfNotes.filter(currNote => currNote._id !== action.payload._id) ]
+            case "PIN_THE_NOTE":
+                return [ ...stateOfNotes.map(currNote => currNote._id === action.payload._id? {...currNote, isPinned: true} : currNote ) ]
+            case "UNPIN_THE_NOTE":
+                return [ ...stateOfNotes.map(currNote => currNote._id === action.payload._id? {...currNote, isPinned: false} : currNote ) ]
         }
     }
 
